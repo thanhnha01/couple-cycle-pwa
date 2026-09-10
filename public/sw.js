9728859b9af8e53bddd8708f6fdfe863c6e0ae46
thanhnha01 <nguyennha.020201@gmail.com>
2026-09-10 18:00:57 +0700

    feat: refresh couple home and add love day counter

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "nhip-doi-shell-v5";
+const CACHE_VERSION = "nhip-doi-shell-v6";
 const BASE_URL = new URL("./", self.location.href);
 const assetUrl = (path) => new URL(path, BASE_URL).pathname;
 const APP_SHELL = [
